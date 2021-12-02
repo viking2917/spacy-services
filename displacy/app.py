23bc2744448cdf12aff59fde78c4f3b09e969aa5
Mark Watkins <mark.watkins1@gmail.com>
2021-12-02 05:24:18 -1000

    update app.py
    CORS middleware is now part of the core package, no need for separate.
    return the text of the entities as well as position and label_ type.

--- displacy/app.py
+++ displacy/app.py
@@ -2,24 +2,24 @@
 from __future__ import unicode_literals
 
 import hug
-from hug_middleware_cors import CORSMiddleware
+from hug.middleware import CORSMiddleware
 import spacy
 
 
 print("Loading...")
 MODELS = {
     "en_core_web_sm": spacy.load("en_core_web_sm"),
-    "en_core_web_md": spacy.load("en_core_web_md"),
-    "en_core_web_lg": spacy.load("en_core_web_lg"),
-    "de_core_news_sm": spacy.load("de_core_news_sm"),
-    "es_core_news_sm": spacy.load("es_core_news_sm"),
-    "pt_core_news_sm": spacy.load("pt_core_news_sm"),
-    "fr_core_news_sm": spacy.load("fr_core_news_sm"),
-    "it_core_news_sm": spacy.load("it_core_news_sm"),
-    "nl_core_news_sm": spacy.load("nl_core_news_sm"),
-    "nb_core_news_sm": spacy.load("nb_core_news_sm"),
-    "lt_core_news_sm": spacy.load("lt_core_news_sm"),
-    'el_core_news_sm': spacy.load('el_core_news_sm'),
+    # "en_core_web_md": spacy.load("en_core_web_md"),
+    # "en_core_web_lg": spacy.load("en_core_web_lg"),
+    # "de_core_news_sm": spacy.load("de_core_news_sm"),
+    # "es_core_news_sm": spacy.load("es_core_news_sm"),
+    # "pt_core_news_sm": spacy.load("pt_core_news_sm"),
+    # "fr_core_news_sm": spacy.load("fr_core_news_sm"),
+    # "it_core_news_sm": spacy.load("it_core_news_sm"),
+    # "nl_core_news_sm": spacy.load("nl_core_news_sm"),
+    # "nb_core_news_sm": spacy.load("nb_core_news_sm"),
+    # "lt_core_news_sm": spacy.load("lt_core_news_sm"),
+    # 'el_core_news_sm': spacy.load('el_core_news_sm'),
 }
 print("Loaded!")
 
@@ -63,7 +63,7 @@ def ent(text: str, model: str):
     nlp = MODELS[model]
     doc = nlp(text)
     return [
-        {"start": ent.start_char, "end": ent.end_char, "label": ent.label_}
+        {"text": ent.text, "start": ent.start_char, "end": ent.end_char, "label": ent.label_}
         for ent in doc.ents
     ]
 
